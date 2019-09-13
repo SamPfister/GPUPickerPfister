a3103bb568c2534ce6a7cd5ad31f3e8b8222db30
SamPfister <Sam.Pfister.550@gmail.com>
2019-09-13 13:50:13 -0500

    2nd commit

--- app/src/main/java/com/example/gpupicker/MainActivity.java
+++ app/src/main/java/com/example/gpupicker/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
@@ -13,13 +17,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements OnItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements OnItemSelectedListener {
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private Button btnDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // radio button code from https://www.mkyong.com/android/android-radio-buttons-example/
         // spinner code from https://developer.android.com/guide/topics/ui/controls/spinner
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
+
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         // Spinner click listener
         spinner.setOnItemSelectedListener(this);
@@ -32,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         spinner.setAdapter(adapter);
 
     }
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         // On selecting a spinner item
@@ -40,8 +51,37 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         // Showing selected spinner item
         Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
     }
+
     public void onNothingSelected(AdapterView<?> arg0) {
-        // TODO Auto-generated method stub
+        Toast.makeText(MainActivity.this, "Select a brand", Toast.LENGTH_SHORT).show();
+    }
+    public void addListenerOnButton() {
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        btnDisplay = (Button) findViewById(R.id.button);
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get selected radio button from radioGroup
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                // find the radiobutton by returned id
+                radioButton = (RadioButton) findViewById(selectedId);
+                Toast.makeText(MainActivity.this, radioButton.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.checkBox1:
+                if (checked){
+                    Toast.makeText(MainActivity.this, "check box 1 checked", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            // TODO: Veggie sandwich
+        }
     }
 }
 
