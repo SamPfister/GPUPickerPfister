195ad8d068b40df82d72167338f4ca4b2b5acad8
SamPfister <Sam.Pfister.550@gmail.com>
2019-09-16 13:49:55 -0500

    I added a new activity and other things

--- app/src/main/java/com/example/gpupicker/MainActivity.java
+++ app/src/main/java/com/example/gpupicker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gpupicker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,6 +11,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -61,6 +63,7 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         btnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 // get selected radio button from radioGroup
                 int selectedId = radioGroup.getCheckedRadioButtonId();
                 // find the radiobutton by returned id
@@ -80,8 +83,73 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
                     Toast.makeText(MainActivity.this, "check box 1 checked", Toast.LENGTH_SHORT).show();
                 }
                 break;
-            // TODO: Veggie sandwich
+            case R.id.checkBox2:
+                if (checked){
+                    Toast.makeText(MainActivity.this, "check box 2 checked", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.checkBox3:
+                if (checked){
+                    Toast.makeText(MainActivity.this, "check box 3 checked", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.checkBox4:
+                if (checked){
+                    Toast.makeText(MainActivity.this, "check box 4 checked", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.checkBox5:
+                if (checked){
+                    Toast.makeText(MainActivity.this, "check box 5 checked", Toast.LENGTH_SHORT).show();
+                }
+                break;
         }
     }
+
+
+    public void switchClick(View view){
+        Switch simpleSwitch = (Switch) findViewById(R.id.switch1);
+        boolean switchState = simpleSwitch.isChecked();
+
+        if(switchState){
+            Toast.makeText(MainActivity.this, "The switch is on", Toast.LENGTH_SHORT).show();
+            switchState = false;
+        }
+        else{
+            Toast.makeText(MainActivity.this, "The switch is off", Toast.LENGTH_SHORT).show();
+            switchState = true;
+        }
+    }
+
+    public void sendInfo(View v){
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+        radioButton = (RadioButton) findViewById(selectedId);
+        String rgText = (String) radioButton.getText();
+        CheckBox cb1 = (CheckBox) findViewById(R.id.checkBox1);
+        CheckBox cb2 = (CheckBox) findViewById(R.id.checkBox2);
+        CheckBox cb3 = (CheckBox) findViewById(R.id.checkBox3);
+        CheckBox cb4 = (CheckBox) findViewById(R.id.checkBox4);
+        CheckBox cb5 = (CheckBox) findViewById(R.id.checkBox5);
+        boolean cbval1 = cb1.isChecked();
+        boolean cbval2 = cb2.isChecked();
+        boolean cbval3 = cb3.isChecked();
+        boolean cbval4 = cb4.isChecked();
+        boolean cbval5 = cb5.isChecked();
+        Switch switch1 = (Switch) findViewById(R.id.switch1);
+        boolean switchval = switch1.isChecked();
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        String spinnerval = spinner.getSelectedItem().toString();
+        Intent intent = new Intent(this, output.class);
+        intent.putExtra(output.RG_TEXT, rgText);
+        intent.putExtra(output.CB1_VAL, cbval1);
+        intent.putExtra(output.CB2_VAL, cbval2);
+        intent.putExtra(output.CB3_VAL, cbval3);
+        intent.putExtra(output.CB4_VAL, cbval4);
+        intent.putExtra(output.CB5_VAL, cbval5);
+        intent.putExtra(output.SWITCH_VAL, switchval);
+        intent.putExtra(output.SPINNER_VAL, spinnerval);
+    }
+
 }
 
